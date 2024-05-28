4d8b08a59494a8d394f324029eb20a0f0e78b6df
pumkko <c.coque@hotmail.fr>
2024-05-28 12:10:25 -0400

    believed my way was better did not use a match if) but anyway: ctrl+q to quit

--- src/editor.rs
+++ src/editor.rs
@@ -1,33 +1,49 @@
 use crossterm::{
-    event::{read, Event, KeyCode},
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor {}
+        Editor { should_quit: false }
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(e) = self.repl() {
             panic!("{e:#?}");
         }
         println!("Goodbye. \r");
     }
 
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
 
         loop {
-            if let Event::Key(key_event) = read()? {
-                println!("{key_event:?} \r");
-                if let KeyCode::Char(c) = key_event.code {
-                    if c == 'q' {
-                        break;
+            if let Event::Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                println!(
+                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
+                );
+
+                match code {
+                    KeyCode::Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
                     }
+                    _ => (),
                 }
+            }
+
+            if self.should_quit {
+                break;
             }
         }
 
