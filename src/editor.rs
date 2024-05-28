797eec7a7a0d2facc0dada0fa0f7b5378af6d507
pumkko <c.coque@hotmail.fr>
2024-05-28 11:21:30 -0400

    move editor logic into its own file

---
+++ src/editor.rs
@@ -0,0 +1,38 @@
+use std::io::Read;
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor {}
+    }
+
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+
+        for b in std::io::stdin().bytes() {
+            match b {
+                Ok(u8_value) => {
+                    let char_value = u8_value as char;
+
+                    if char_value.is_control() {
+                        println!("Binary {0:08b}, ASCII: {0:#3} \r", u8_value);
+                    } else {
+                        println!(
+                            "Binary {0:08b}, ASCII: {0:#3} Character {1:#?}\r",
+                            u8_value, char_value
+                        );
+                    }
+
+                    if char_value == 'q' {
+                        break;
+                    }
+                }
+                Err(err) => println!("Error : {}", err),
+            }
+        }
+        disable_raw_mode().unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
-use std::io::Read;
+mod editor;
+use editor::Editor;
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+
 
 fn main() {
-    enable_raw_mode().unwrap();
-
-    for b in std::io::stdin().bytes() {
-        match b {
-            Ok(u8_value) => {
-                let char_value = u8_value as char;
-
-                if char_value.is_control() {
-                    println!("Binary {0:08b}, ASCII: {0:#3} \r", u8_value);
-                } else {
-                    println!(
-                        "Binary {0:08b}, ASCII: {0:#3} Character {1:#?}\r",
-                        u8_value, char_value
-                    );
-                }
-
-                if char_value == 'q' {
-                    break;
-                }
-            }
-            Err(err) => println!("Error : {}", err),
-        }
-    }
-    disable_raw_mode().unwrap();
+   let editor = Editor::default();
+   editor.run();
 }
