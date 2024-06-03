5f842d1e1fff1b692316c8baa3da6c1fc2c1895b
pumkko <c.coque@hotmail.fr>
2024-06-03 14:13:38 -0400

    add move cursor logic

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,9 @@
-use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::event::{
+    read,
+    Event::{self, Key},
+    KeyCode::{self, Char},
+    KeyEvent, KeyModifiers,
+};
 use terminal::Terminal;
 
 mod terminal;
@@ -11,17 +16,17 @@ impl Editor {
     pub fn default() -> Self {
         Editor { should_quit: false }
     }
-    pub fn run(&mut self) {
-        Terminal::initialize().unwrap();
-        let result = self.repl();
-        Terminal::terminate().unwrap();
-        result.unwrap();
+    pub fn run(&mut self) -> std::io::Result<()> {
+        Terminal::initialize()?;
+        self.repl()?;
+        Terminal::terminate()?;
+        Ok(())
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
             let event = read()?;
-            self.evaluate_event(&event);
+            self.evaluate_event(&event)?;
             self.refresh_screen()?;
             if self.should_quit {
                 break;
@@ -29,7 +34,7 @@ impl Editor {
         }
         Ok(())
     }
-    fn evaluate_event(&mut self, event: &Event) {
+    fn evaluate_event(&mut self, event: &Event) -> std::io::Result<()> {
         if let Key(KeyEvent {
             code, modifiers, ..
         }) = event
@@ -38,9 +43,15 @@ impl Editor {
                 Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.should_quit = true;
                 }
+                KeyCode::Up => Terminal::cursor_move_up()?,
+                KeyCode::Down => Terminal::cursor_move_down()?,
+                KeyCode::Left => Terminal::cursor_move_left()?,
+                KeyCode::Right => Terminal::cursor_move_right()?,
                 _ => (),
             }
         }
+
+        Ok(())
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,7 +1,8 @@
 use std::io::stdout;
 
 use crossterm::{
-    cursor, execute,
+    cursor::{self, MoveToNextLine},
+    execute, queue, style,
     terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
 
@@ -13,11 +14,13 @@ impl Terminal {
         Self::clear_screen()?;
         let (_, rows) = terminal::size()?;
 
+        let mut stdout = stdout();
+
         for _ in 0..rows {
-            print!("~\r\n");
+            queue!(stdout, style::Print("~"), MoveToNextLine(1))?;
         }
 
-        execute!(stdout(), cursor::MoveTo(1, 0))
+        execute!(stdout, cursor::MoveTo(1, 0))
     }
 
     pub fn terminate() -> Result<(), std::io::Error> {
@@ -26,7 +29,22 @@ impl Terminal {
 
     pub fn clear_screen() -> Result<(), std::io::Error> {
         let mut stdout = stdout();
-        execute!(stdout, cursor::MoveTo(0, 0))?;
-        execute!(stdout, Clear(ClearType::All))
+        execute!(stdout, cursor::MoveTo(0, 0), Clear(ClearType::All))
+    }
+
+    pub fn cursor_move_up() -> std::io::Result<()> {
+        execute!(stdout(), cursor::MoveUp(1))
+    }
+
+    pub fn cursor_move_down() -> std::io::Result<()> {
+        execute!(stdout(), cursor::MoveDown(1))
+    }
+
+    pub fn cursor_move_right() -> std::io::Result<()> {
+        execute!(stdout(), cursor::MoveRight(1))
+    }
+    
+    pub fn cursor_move_left() -> std::io::Result<()> {
+        execute!(stdout(), cursor::MoveLeft(1))
     }
 }
